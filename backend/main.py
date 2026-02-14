2bea209c83803cb916267b4543853d91e4e89746
Kaustubh1835 <kaustubhbodakhe1835@gmail.com>
2026-02-14 18:36:25 +0530

    adding frontned url in backend for CORS error

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # CORS — allow Next.js frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://pdf-rag-plum.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
